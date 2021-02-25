d77ea0f3a82d3ddc0a58fdc08784421d050a7624
Hu Yong An <Hu Yong An@192.168.0.109>
2021-02-25 13:41:23 +0100

    Stampa la lunghezza della stringa ottenuto dal server

--- src/clientbase/ClientBase.java
+++ src/clientbase/ClientBase.java
@@ -10,39 +10,33 @@ import java.util.logging.Logger;
 public class ClientBase {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a;
+        String[] a;
         
-        while (true) {
-            System.out.print("Inserire una parola: ");
-            a = sc.next();
-            
-            System.out.println("Apertua connessione");
-            try {
-                Socket server = new Socket("127.0.0.1", 5500);
-                OutputStream alServer = server.getOutputStream();
-                BufferedWriter scrittore = new BufferedWriter(new OutputStreamWriter(alServer));
-                
-                scrittore.write(a);
-                
-                InputStream dalServer = server.getInputStream();
-                BufferedReader lettore = new BufferedReader(new InputStreamReader(dalServer));
+        System.out.println("Apertua connessione");
+        try {
+            Socket server = new Socket("127.0.0.1", 5500);
 
-                String risposta = lettore.readLine();
+            PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            Scanner tastiera = new Scanner(System.in);
+            String s = "";
+            
+            while (!s.equals("exit")) {
+                System.out.println("Inserire frase da inviare al server:");
+                s = tastiera.nextLine();
+                System.out.println("frase inserita: " + s);
+                out.println(s);
+                String risposta = in.readLine();
                 System.out.println("risposta del server: " + risposta);
-                
-                
-                scrittore.close();
-                lettore.close();
-                server.close();
-                System.out.println("chiusura connessione");
-                Thread.sleep(5000);
-            } catch (IOException ex) {
-                Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
+                a = risposta.split(" ");
+                System.out.println("Hai inserito "+a[a.length-1]);
             }
+            
+            in.close();
+            server.close();
+            System.out.println("chiusura connessione");
+        } catch (IOException ex) {
+            Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
 }
