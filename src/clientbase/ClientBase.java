0653117a24a324328edbcab5f1e5304548a62f30
hu.yongan <hu.yongan@TAVE-PC20.OSDBSESTO.local>
2021-03-04 14:04:11 +0100

    Sends an int to the server

--- src/clientbase/ClientBase.java
+++ src/clientbase/ClientBase.java
@@ -10,29 +10,18 @@ import java.util.logging.Logger;
 public class ClientBase {
 
     public static void main(String[] args) {
-        String[] a;
-        
         System.out.println("Apertua connessione");
         try {
             Socket server = new Socket("127.0.0.1", 5500);
-
+            
             PrintWriter out = new PrintWriter(server.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
             Scanner tastiera = new Scanner(System.in);
-            String s = "";
+            int n;
             
-            while (!s.equals("exit")) {
-                System.out.println("Inserire frase da inviare al server:");
-                s = tastiera.nextLine();
-                System.out.println("frase inserita: " + s);
-                out.println(s);
-                String risposta = in.readLine();
-                System.out.println("risposta del server: " + risposta);
-                a = risposta.split(" ");
-                System.out.println("Hai inserito "+a[a.length-1]);
-            }
+            System.out.print("Inserire un numero: ");
+            n = tastiera.nextInt();
+            out.println(n);
             
-            in.close();
             server.close();
             System.out.println("chiusura connessione");
         } catch (IOException ex) {
